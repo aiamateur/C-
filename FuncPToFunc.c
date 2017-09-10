1afecffefb413733d0b5e2abf42d5492dee07c10
aiamateur <rashmiravinathan@gmail.com>
2017-09-10 12:05:12 +0530

    Create FuncPToFunc.c
    Passing function pointer to function

---
+++ FuncPToFunc.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+int compare1(int i, int j)
+{
+    printf("In compare1\n");
+    return 1;
+
+}
+
+int compare2(int i, int j)
+{
+    printf("In compare2\n");
+    return 1;
+
+}
+
+void whichFunction(int i, int j, int (* compare)(int, int))
+{
+    if(i > j)
+    {
+        compare(i, j);
+        printf("Called compare1\n");
+    }else
+    {
+        compare(i, j);
+        printf("Called compare2\n");    
+    }
+}
+
+typedef int (* compare)(int, int);
+
+
+int main() {
+    compare ctype1 = compare1;
+    compare ctype2 = compare2;
+     
+       
+
+   int x=10;
+   int y=25;
+   whichFunction(x, y, ctype1);
+   
+   return 0;
+}
