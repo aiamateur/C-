42fd6a97f346acca2403f45271f335831c46a6f4
aiamateur <rashmiravinathan@gmail.com>
2017-09-17 19:25:03 +0530

    Create 2dArrayTemplate.c

---
+++ 2dArrayTemplate.c
@@ -0,0 +1,62 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int** create_int_matrix(int nrows, int ncols)
+{
+    int** m = malloc(nrows*sizeof(int*));
+    for(int i = 0; i < ncols; i++)
+    {
+        m[i] = malloc(ncols*sizeof(int));
+    }
+    
+    return m;
+
+}
+
+void free_int_matrix(int** m, int nrows)
+{
+    for(int i = 0; i < nrows; i++)
+    {
+        free(m[i]);
+    }
+    
+    free(m);
+
+}
+
+void print_int_matrix(int** m, int nrows, int ncols)
+{
+    for(int i = 0; i < nrows; i++)
+    {
+        for (int j = 0; j< ncols; j++)
+        {
+            printf("%.2d ", m[i][j]);
+        }
+        printf("\n");
+    }
+
+
+}
+
+
+int main()
+{
+    int nrows = 10;
+    int ncols = 10;
+    int** m;
+    
+    m = create_int_matrix(nrows, ncols);
+    
+    for (int i = 0; i < nrows; i++)
+    {
+        for (int j = 0; j < ncols; j++)
+        {
+            m[i][j] = i*j;
+        }
+    }
+    
+    print_int_matrix(m, nrows, ncols);
+    
+    free_int_matrix(m, nrows);
+
+}
